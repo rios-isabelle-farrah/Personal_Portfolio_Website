d855d9c3324945e5b95514375348432d660edd57
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2021-03-10 14:25:36 -0500

    add video of game to projects page

--- matrix.js
+++ matrix.js
@@ -1,29 +1,23 @@
 let menu = document.querySelector("#hamburger-menu");//
 let dropDown = document.querySelector("#drop-down");//
 let homeLi = document.querySelector('#home-li')
-let projectLi = document.querySelector('#project-li')
+let projectLi = document.querySelector('#projects-li')
 let aboutLi = document.querySelector('#about-li')
 let contactLi = document.querySelector('#contact-li')
 let x = document.querySelector('#xb')
-let revert = document.createElement('button')
-revert.id = 'no-rain'
-revert.textContent = "revert"
+
 
 menu.addEventListener('mouseover', (event) => {
    dropDown.style.display = 'block' 
     })
 
-
+//this event listener makes the dropdown menu disappear by using display none.
 let xButton = document.querySelector('#xb')
    xButton.addEventListener('click', (event) => {
            dropDown.style.display = 'none' 
     })
-
-
-
    let makeRain = document.getElementById("no-matrix");
    let makeRainButton = document.getElementById("rain");
-   let revertButton = document.getElementById("revert");
    
    makeRainButton.addEventListener('click', (event) => {
        makeRain.classList.toggle("matrix");
@@ -34,36 +28,34 @@ let xButton = document.querySelector('#xb')
            let plainPic = document.getElementById('plain-pic')
            divOfPic.removeChild(switchPic)
            plainPic.style.display = 'block'
-        //    document.getElementById('name').style.color = 'limegreen'
-        //    document.getElementById('title').style.color = 'limegreen'
-        //    document.getElementById('pursuit').style.color = 'limegreen'
        
-          
-        
-          
-           
         } else {
             makeRainButton.innerHTML = "dark mode";
             let matrixPic = document.createElement("IMG")
             matrixPic.id = 'matrix-pic';
             matrixPic.setAttribute("src", "matrix.png");
-            matrixPic.setAttribute("width", "200");
-            matrixPic.setAttribute("height", "228");
+            matrixPic.setAttribute("width", "20");
+            matrixPic.setAttribute("height", "28");
             matrixPic.setAttribute("alt", "Farrah's Matric Pic");
             let divOfPic = document.getElementById('pic')
             divOfPic.appendChild(matrixPic);
             let plainPic = document.getElementById('plain-pic')
-            plainPic.style.display = 'none'
-        //   document.getElementById('name').style.color = 'white'
-        //     document.getElementById('title').style.color = 'white'
-        //     document.getElementById('pursuit').style.color = 'white'
-            
+           plainPic.style.display = 'none' 
+        }
+ })
+    
 
-     }
-       
-    })
-    
-    
+let watchButton = document.querySelector('#watch')
+ watchButton.addEventListener('click', (event) => {
+let cover = document.getElementById('snow-video-cover')
+// cover.classList.toggle("cover-away");
+cover.style.display = 'none'
+
+setTimeout(function () {
+    cover.style.display = 'block'
+        }, 15000);
+
+  })
   
 
 
